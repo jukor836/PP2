bb28d08e125c9a5b16d22effd7377b541f098edf
jullia <126663096+jukor836@users.noreply.github.com>
2023-03-23 19:46:11 +0700

    Update pp5.cpp

--- pp2/pp5.cpp
+++ pp2/pp5.cpp
@@ -7,46 +7,59 @@ using namespace std;
 #define err_exit(code, str) { cerr << str << ": " << strerror(code) \
 << endl; exit(EXIT_FAILURE); }
 pthread_mutex_t mutex;
-pthread_cond_t cond;
 bool state = false;
+bool state2=true;
+void signal()
+{
+    state=true;
+}
+void wait()
+{
+    int err;
+    err = pthread_mutex_unlock(&mutex);
+    if(err != 0)
+        err_exit(err, "Cannot unlock mutex");
+    while(!state) ;
+    state=false;
+}
 void *thread_job(void *arg)
 {
     int err;
-
-  
-        // Захватываем мьютекс 
-        err = pthread_mutex_lock(&mutex);
+    
+    // Захватываем мьютекс 
+    err = pthread_mutex_lock(&mutex);
     if(err != 0)
-        err_exit(err, "Cannot send signal");
-        if(err != 0)
-            err_exit(err, "Cannot lock mutex");
-        while(!state) {
-            cout<<"созданный поток"<< pthread_self()<<" ждет сигнала "<<endl;
-            err = pthread_cond_wait(&cond, &mutex);
-            if(err != 0)
-                err_exit(err, "Cannot wait on condition variable");
-            state=true;
-        }
+        err_exit(err, "Cannot lock mutex");
+    state2=false;
+    cout<<"созданный поток "<< pthread_self()<<" ждет сигнала "<<endl;
+    wait();
     cout<<"Поток "<< pthread_self()<<" получил сигнал и продолжил исполнение"<<endl;
-    
-    // Посылаем сигнал
-    
     err = pthread_mutex_unlock(&mutex);
     if(err != 0)
         err_exit(err, "Cannot unlock mutex");
     
-
+    
 }
-
+void *thread_job2(void *arg)
+{
+    while(state2);
+    int err;
+    // Захватываем мьютекс 
+    err = pthread_mutex_lock(&mutex);
+    if(err != 0)
+        err_exit(err, "Cannot lock mutex");
+    cout<<"Подаем сигнал"<<endl;    
+    signal();
+        err = pthread_mutex_unlock(&mutex);
+    if(err != 0)
+        err_exit(err, "Cannot unlock mutex");
+}
 int main()
 {
 int err;
 //
 pthread_t thread1, thread2; // Идентификаторы потоков
-// Инициализируем мьютекс и условную переменную
-err = pthread_cond_init(&cond, NULL);
-if(err != 0)
-err_exit(err, "Cannot initialize condition variable");
+// Инициализируем мьютекс
 err = pthread_mutex_init(&mutex, NULL);
 if(err != 0)
 err_exit(err, "Cannot initialize mutex");
@@ -54,16 +67,14 @@ err_exit(err, "Cannot initialize mutex");
 err = pthread_create(&thread1, NULL, thread_job, NULL);
 if(err != 0)
 err_exit(err, "Cannot create thread 1");
-err = pthread_create(&thread2, NULL, thread_job, NULL);
+
+err = pthread_create(&thread2, NULL, thread_job2, NULL);
 if(err != 0)
-err_exit(err, "Cannot create thread 1");
-while(!state)
-    err = pthread_cond_signal(&cond);
+err_exit(err, "Cannot create thread 2");
+
 // Дожидаемся завершения потоков
 pthread_join(thread1, NULL);
-
+pthread_join(thread2, NULL);
 // Освобождаем ресурсы, связанные с мьютексом
-// и условной переменной
 pthread_mutex_destroy(&mutex);
-pthread_cond_destroy(&cond);
 }
