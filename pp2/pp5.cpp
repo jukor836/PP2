a2a3e34806dbe779a48e9a9bc5e3112a02799a89
jullia <126663096+jukor836@users.noreply.github.com>
2023-04-13 17:20:27 +0700

    Update pp5.cpp

--- pp2/pp5.cpp
+++ pp2/pp5.cpp
@@ -7,20 +7,66 @@ using namespace std;
 #define err_exit(code, str) { cerr << str << ": " << strerror(code) \
 << endl; exit(EXIT_FAILURE); }
 pthread_mutex_t mutex;
-bool state = false;
-bool state2=true;
-void signal()
+bool state = true,state2=true;
+// Тип условной переменной
+typedef struct CondVar
 {
-    state=true;
+    pthread_mutex_t mutex;
+} CondVar;
+
+CondVar cond;
+// Инициализация условной переменнной
+void cond_init(CondVar *cond)
+{
+    int err = pthread_mutex_init(&cond->mutex, NULL);
+    if (err != 0)
+        err_exit(err,"Error: cannot initialize mutex!");
 }
-void wait()
+// Уничтожение условной переменнной
+void cond_destroy(CondVar *cond)
 {
-    int err;
-    err = pthread_mutex_unlock(&mutex);
-    if(err != 0)
-        err_exit(err, "Cannot unlock mutex");
-    while(!state) ;
-    state=false;
+    pthread_mutex_destroy(&cond->mutex);
+}
+// Блокировка до наступления события
+void wait(CondVar *cond, pthread_mutex_t *mutex)
+{
+    // Блокируем условную переменную
+    int err = pthread_mutex_lock(&cond->mutex);
+    if (err != 0)
+        err_exit(err,"Error: cannot lock condition mutex!");
+    state=true;
+    // Освобождаем условную переменную
+    err = pthread_mutex_unlock(&cond->mutex);
+    if (err != 0)
+        err_exit(err,"Error: cannot unlock condition mutex!");
+
+
+    // Освобождаем мьютекс до получения сигнала
+    err = pthread_mutex_unlock(mutex);
+    if (err != 0)
+        err_exit(err,"Error: cannot unlock mutex!");
+
+
+    // Ждём пока не получен сигнал
+    while (state);
+
+    // Блокируем мьютекс после получения сигнала
+    err = pthread_mutex_lock(mutex);
+    if (err != 0)
+        err_exit(err,"Error: cannot lock mutex!");
+}
+// Сигнал для выхода из блокировки одного потока
+void signal(CondVar *cond)
+{
+    // Блокируем условную переменную
+    int err = pthread_mutex_lock(&cond->mutex);
+    if (err != 0)
+        err_exit(err,"Error: cannot lock condition mutex!");
+    state= false;
+    // Освобождаем условную переменную
+    err = pthread_mutex_unlock(&cond->mutex);
+    if (err != 0)
+        err_exit(err,"Error: cannot unlock condition mutex!");
 }
 void *thread_job(void *arg)
 {
@@ -32,7 +78,7 @@ void *thread_job(void *arg)
         err_exit(err, "Cannot lock mutex");
     state2=false;
     cout<<"созданный поток "<< pthread_self()<<" ждет сигнала "<<endl;
-    wait();
+    wait(&cond,&mutex);
     cout<<"Поток "<< pthread_self()<<" получил сигнал и продолжил исполнение"<<endl;
     err = pthread_mutex_unlock(&mutex);
     if(err != 0)
@@ -49,7 +95,7 @@ void *thread_job2(void *arg)
     if(err != 0)
         err_exit(err, "Cannot lock mutex");
     cout<<"Подаем сигнал"<<endl;    
-    signal();
+    signal(&cond);
         err = pthread_mutex_unlock(&mutex);
     if(err != 0)
         err_exit(err, "Cannot unlock mutex");
@@ -63,6 +109,7 @@ pthread_t thread1, thread2; // Идентификаторы потоков
 err = pthread_mutex_init(&mutex, NULL);
 if(err != 0)
 err_exit(err, "Cannot initialize mutex");
+cond_init(&cond);
 // Создаём потоки
 err = pthread_create(&thread1, NULL, thread_job, NULL);
 if(err != 0)
@@ -75,6 +122,7 @@ err_exit(err, "Cannot create thread 2");
 // Дожидаемся завершения потоков
 pthread_join(thread1, NULL);
 pthread_join(thread2, NULL);
+cond_destroy(&cond);
 // Освобождаем ресурсы, связанные с мьютексом
 pthread_mutex_destroy(&mutex);
 }
